921aa1a1b9b456e158b22700568b718b866fe50f
Francisco Vargas <fralva86@gmail.com>
2023-09-05 12:05:48 -0600

    weed out unnecesary content

--- js/main.js
+++ js/main.js
@@ -18,8 +18,6 @@ menuBtnContainer.addEventListener("click", function () {
 
 function myFunction(imgs) {
   var expandImg = document.getElementById("expandedImg");
-  var fullImage = document.querySelectorAll(".fullSize");
-  var imgText = document.getElementById("imgtext");
   expandImg.src = imgs.src;
   expandImg.parentElement.style.display = "block";
 }
